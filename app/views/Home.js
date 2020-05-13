b7d9e58ea609b46ff43f39e15be9de2713166671
Hansen Dsilva <hansen.dsilva@sap.com>
2020-05-13 23:33:05 +0530

    intiial commit witth app/views/home.js

---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Home extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>Hey there, this is Home.js</Text>
+                <Text>I hope you are well</Text>
+                <Text>I am well too</Text>
+            </View>
+        );
+    }
+}
